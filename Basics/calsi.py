49542d4d0b7191e0c1e3a50a4ea93d0adef77d05
vijay-doddi <vijaydoddi07@gmail.com>
2026-06-30 19:06:29 +0530

    Calculator
    Added a simple calculator with addition, subtraction, and multiplication functionalities. The program prompts the user for input until 'End' is entered.

--- Basics/calsi.py
+++ Basics/calsi.py
@@ -1,3 +1,16 @@
-import math 
-print(math.pow(5,4))
-print("End")
+print("Add , Sub , Mul , Div , Sqr , Cube ")
+in=input("Enter Your choice")
+while in!="End":
+  if in=="Add":
+    a=int(input("Enter 1st value ")
+    b=int(input("Enter 2nd value ")
+    print("Addition of these two :",(a+b))
+  if in=="Sub":
+    a=int(input("Enter 1st value ")
+    b=int(input("Enter 2nd value ")
+    print("Substraction of these two :",(a-b))
+  if in=="Mul":
+    a=int(input("Enter 1st value ")
+    b=int(input("Enter 2nd value ")
+    print("Multiplication of these two :",(a*b))
+    
