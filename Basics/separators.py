012cad50000106d144c5a3a255bf9546723b5752
Vijay Doddi <vijaydoddi07@gmail.com>
2026-07-01 13:02:39 +0530

    separators.py

---
+++ Basics/separators.py
@@ -0,0 +1,11 @@
+
+print("Apple", "Banana", "Mango", sep=", ")
+
+print("10", "20", "30", sep=":")
+
+print("Hello", end=" ")
+print("Python")
+
+print("A", "B", "C", sep="-", end=" End\n")
+
+print("2026", "07", "01", sep="/")
